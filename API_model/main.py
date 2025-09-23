c7ddc427b043fb2a33f1633cc9904e6af8fc8921
Remi Cazelles <remi.cazelles.fr@gmail.com>
2025-09-23 16:29:43 +0200

    change path to load model for the container

--- API_model/main.py
+++ API_model/main.py
@@ -3,7 +3,7 @@ import pickle
 from pydantic import BaseModel
 
 # Load model once at startup
-with open("../src/model/logistic_regression_model.pkl", "rb") as f:
+with open("./model/logistic_regression_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 # initialize app    
@@ -19,11 +19,4 @@ class BatchPredictRequest(BaseModel):
 @app.post("/predict")
 def predict(request: PredictRequest):
     prediction = model.predict([request.data])
-    return {"prediction": prediction.tolist()}
-
-@app.post("/batch_predict")
-def batch_predict(request: BatchPredictRequest):
-    print(request.data)
-    prediction = model.predict(request.data)
-    print(request.data)
     return {"prediction": prediction.tolist()}
