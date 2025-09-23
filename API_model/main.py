81bde451cada716338c0c12c688b967764b443cf
Remi Cazelles <remi.cazelles.fr@gmail.com>
2025-09-23 16:45:47 +0200

    added logging to monitor model usage

--- API_model/main.py
+++ API_model/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 import pickle
 from pydantic import BaseModel
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 # Load model once at startup
 with open("./model/logistic_regression_model.pkl", "rb") as f:
@@ -16,7 +19,25 @@ class BatchPredictRequest(BaseModel):
     data: list[list[float]]
 
 
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
 @app.post("/predict")
 def predict(request: PredictRequest):
-    prediction = model.predict([request.data])
-    return {"prediction": prediction.tolist()}
+    logging.info(f"Prediction request: {request.data}")
+
+    try:
+        prediction = model.predict([request.data])
+        logging.info(f"Prediction result: {prediction}")
+        return {"prediction": prediction.tolist()}
+
+    except:
+        logging.info(f"Prediction Failed")
+        return {"prediction": "failed to predict"}
+        
+    
+
+
